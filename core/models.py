d60399a6ab36df9a1c6afe6f6a610b7357d2e398
mmislam <mmislam272@gmail.com>
2024-10-29 23:12:56 +0600

    user manager setup  at core

--- core/models.py
+++ core/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None,**extras):
+        user=self.model(email,**extras)
+        user.set_password(password)
 
+        user.save()
+
+        return user
+    
+
+    
