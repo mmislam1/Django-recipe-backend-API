c9e3044801efc7f8f02705b41c63b878f46eacf7
mmislam <mmislam272@gmail.com>
2024-11-02 22:04:25 +0600

    db setting

--- app/settings.py
+++ app/settings.py
@@ -104,11 +104,8 @@ DATABASES = {
 =======
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydb',
@@ -119,7 +116,6 @@ DATABASES = {
 >>>>>>> 4e6b3bb (docker)
     }
 }
-
 
 # Password validation
 <<<<<<< HEAD
